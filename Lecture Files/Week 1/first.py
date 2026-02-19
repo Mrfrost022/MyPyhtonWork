c902a2f9563f00389f4ce8cca53bb67d60d06544
Prashant Singh <prashant.singhfamily@gmail.com>
2026-02-19 16:38:51 +0530

    number is even or odd

--- Lecture Files/Week 1/first.py
+++ Lecture Files/Week 1/first.py
@@ -1,1 +1,8 @@
-print("hello world!")
+# code to check number is even or odd
+number=int(input("Enter the number here"))
+if (number%2==0):
+    print(number, "is Even")
+elif(number==0):
+    print("This number is neither even nor odd")
+else:
+    print(number, "is odd")
