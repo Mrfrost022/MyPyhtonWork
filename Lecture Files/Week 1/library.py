292616916736c0448d544a981a9a7f102cc04b3b
Prashant Singh <prashant.singhfamily@gmail.com>
2026-02-20 14:19:44 +0530

    Code for importing calendar library by all the types it could be

---
+++ Lecture Files/Week 1/library.py
@@ -0,0 +1,18 @@
+# This is simple code to print the calendar of a month using calendar module in python
+import calendar
+print(calendar.month(2024, 4))
+
+# This is simple code to print the calendar of a year using calendar module in python
+import calendar
+print(calendar.calendar(2026))
+
+# how to call all the funtionso calendar module in your python code
+'''
+this code will print the calendar of April 2024 using the month function from the calendar module.
+You can also use the calendar function to print the calendar of a whole year. For example:'''
+from calendar import *
+print(month(2024, 4))
+
+#importing library as small name
+import calendar as c
+print(c.month(2024, 4))
