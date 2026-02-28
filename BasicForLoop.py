a13fe0f527a8bd3bd75efd88206a47fdf327462c
Prashant Singh <prashant.singhfamily@gmail.com>
2026-03-01 00:16:56 +0530

    Basics of for loop- end parameter

---
+++ BasicForLoop.py
@@ -0,0 +1,6 @@
+#for printing in same line we can use end parameter in print function
+for i in range (1,11):
+    print(i,end=" ")
+'''This will print all numbers from 1 to 10 like
+1 2 3 4 5 6  7 8 9 10 because we have given end parameter as space in print function'''
+'''
