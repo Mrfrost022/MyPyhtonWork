d28dda3dcc9d72d3cc974d854cd1237fbb173ff1
Prashant Singh <prashant.singhfamily@gmail.com>
2026-02-28 15:11:42 +0530

    Also add same code using while loop

--- reverse_digit.py
+++ reverse_digit.py
@@ -1,4 +1,4 @@
-# Code for reversing the digit of a number
+'''# Code for reversing the digit of a number
 num=input("Enter a number: ")
 if int(num)>=0:
     rev_num=int(num[::-1])
@@ -7,3 +7,23 @@ else:
     num=num[1:]
     rev_num=int(num[::-1])
     print("The reverse of the given number is", rev_num*(-1))
+'''
+
+#code for reversing the digit of a number using while loop
+num=int(input("Enter a number: "))
+rev_num=0
+if num<0:
+    num=num*(-1)
+    while num>0:
+        l=num%10
+        rev_num=rev_num*10+l
+        num=num//10
+    rev_num=rev_num*(-1)
+elif num==0:
+    rev_num=0
+else:
+    while num>0:
+        l=num%10
+        rev_num=rev_num*10+l
+        num=num//10
+print("The reverse of the given number is", rev_num)
