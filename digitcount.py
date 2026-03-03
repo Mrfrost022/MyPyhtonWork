d0f068dd1619fc04dd34887d2ad0f0f7d2ec9627
Prashant Singh <prashant.singhfamily@gmail.com>
2026-03-03 17:40:43 +0530

    Added digit count code using for loop also

--- digitcount.py
+++ digitcount.py
@@ -9,3 +9,12 @@ while num>0:
     count=count+1
     num=num//10
 print("The number of digits in the given number is", count)
+
+#code for counting the number of digits in the given number using for loop
+num=int(input("Enter a number: "))
+count=0
+if num<0:
+    num=num*(-1)
+for i in str(num):
+    count=count+1
+print("The number of digits in the given number is", count)
